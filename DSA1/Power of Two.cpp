73fa1d8a3097c1543f2296ae984caa8e9cb2090b
Armaan Nayyar <72308743+alphaninja27@users.noreply.github.com>
2022-03-14 12:58:06 +0530

    Create Power of Two.cpp

---
+++ DSA1/Power of Two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        long ans = 1;
+        for(int i=0;i<=30;i++){
+            if(ans == n)
+                return true;
+            ans = ans*2;
+        }
+        return false;
+    }
+};
