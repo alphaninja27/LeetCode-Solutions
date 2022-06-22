ef86820634055a7051a8d491535990553d761196
Armaan Nayyar <72308743+alphaninja27@users.noreply.github.com>
2022-06-23 01:30:04 +0530

    Create Minimum Absolute Difference in BST.py

---
+++ DSA1/Minimum Absolute Difference in BST.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        global ans 
+        ans = []
+        self.help(root)
+        
+        return min([ans[i] - ans[i - 1] for i in range(1,len(ans))])
+    
+    def help(self,root):
+        if root:
+            self.help(root.left)
+            ans.append(root.val)
+            self.help(root.right)
