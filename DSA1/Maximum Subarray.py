347289b8ef150c372db95da81f087f4394ed9978
Armaan Nayyar <72308743+alphaninja27@users.noreply.github.com>
2022-08-18 10:28:32 +0530

    Create Maximum Subarray.py

---
+++ DSA1/Maximum Subarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        sums = 0
+        maxi = -1000000
+        for i in nums:
+            sums += i
+            maxi = max(sums, maxi)
+            if sums < 0:
+                sums = 0
+        return maxi
