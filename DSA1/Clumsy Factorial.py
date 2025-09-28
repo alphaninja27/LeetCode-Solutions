343dd065f8757d1ef68dd7f10b806cbd60f707ab
Armaan Nayyar <72308743+alphaninja27@users.noreply.github.com>
2025-09-28 22:44:42 +0530

    Create Clumsy Factorial.py

---
+++ DSA1/Clumsy Factorial.py
@@ -0,0 +1,16 @@
+class Solution:
+    def clumsy(self, n: int) -> int:
+        if n == 4 or n == 5:
+            return 7
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        elif n == 3:
+            return 6
+        elif n % 4 == 0:
+            return n + 1
+        elif n % 4 == 1 or n % 4 == 2:
+            return n + 2
+        else:
+            return n - 1
