e0bb505744ff3974c5fa6930d8ab1292d457b93d
Armaan Nayyar <72308743+alphaninja27@users.noreply.github.com>
2025-11-09 11:58:27 +0530

    Implement countOperations method to reduce numbers

---
+++ DSA1/Count Operations to Obtain Zero.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        if num1 == 0 or num2 == 0:
+            return 0
+        count = 0
+        while num1 > 0 and num2 > 0:
+            if num1 > num2:
+                num1 -= num2
+                count += 1
+            else:
+                num2 -= num1
+                count += 1
+        return count
+        
