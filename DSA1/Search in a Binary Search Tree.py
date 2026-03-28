0afe79456cf4953634fd1e2186dee6e63f694d02
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2026-03-28 16:52:40 +0530

    Implement search in a Binary Search Tree

---
+++ DSA1/Search in a Binary Search Tree.py
@@ -0,0 +1,11 @@
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        curr = root
+        while curr:
+            if curr.val == val:
+                return curr
+            elif curr.val > val:
+                curr = curr.left
+            else:
+                curr = curr.right
+        return curr
